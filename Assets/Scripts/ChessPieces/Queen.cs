6589c4cb3c2f67b0dcfcbf120c3b960d22c24407
Luke Taylor <luke@dibza.co.uk>
2021-09-08 17:30:05 +0100

    Implment Queen moving through portals

--- Assets/Scripts/ChessPieces/Queen.cs
+++ Assets/Scripts/ChessPieces/Queen.cs
@@ -15,9 +15,46 @@ public class Queen : ChessPiece
                 r.Add(new Vector2Int(currentX, i));
             if (board[currentX, i] != null)
             {
-                if (board[currentX, i].team != team)
+                if (board[currentX, i].team != team && board[currentX, i].type != ChessPieceType.Portal)
                     r.Add(new Vector2Int(currentX, i));
-
+                else if (board[currentX, i].type == ChessPieceType.Portal)
+                {
+                    for (int x = 0; x < tileCountX; x++)
+                    {
+                        for (int y = 0; y < tileCountY; y++)
+                        {
+                            if (x != currentX || y != i)
+                            {
+                                if (board[x, y] != null)
+                                {
+                                    if (board[x, y].type == ChessPieceType.Portal)
+                                    {
+                                        for (int newY = board[x, y].currentY; newY >= 0; newY--)
+                                        {
+                                            if (board[x, newY] == null)
+                                            {
+                                                r.Add(new Vector2Int(x, newY));
+                                            }
+                                            else if (board[x, newY].type != ChessPieceType.Portal) //make sure we dont go through the portal repeatdly like infinite fall in portal
+                                            {
+                                                if (board[x, newY].team != team)
+                                                {
+                                                    r.Add(new Vector2Int(x, newY));
+                                                    goto LoopBreak; //if we hit an enemy we can stop, but we need to leave a few loops so just forcing the exit
+                                                }
+                                                else
+                                                {
+                                                    goto LoopBreak;
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            LoopBreak:
                 break;
             }
         }
@@ -29,9 +66,46 @@ public class Queen : ChessPiece
                 r.Add(new Vector2Int(currentX, i));
             if (board[currentX, i] != null)
             {
-                if (board[currentX, i].team != team)
+                if (board[currentX, i].team != team && board[currentX, i].type != ChessPieceType.Portal)
                     r.Add(new Vector2Int(currentX, i));
-
+                else if (board[currentX, i].type == ChessPieceType.Portal)
+                {
+                    for (int x = 0; x < tileCountX; x++)
+                    {
+                        for (int y = 0; y < tileCountY; y++)
+                        {
+                            if (x != currentX || y != i)
+                            {
+                                if (board[x, y] != null)
+                                {
+                                    if (board[x, y].type == ChessPieceType.Portal)
+                                    {
+                                        for (int newY = board[x, y].currentY; newY < tileCountY; newY++)
+                                        {
+                                            if (board[x, newY] == null)
+                                            {
+                                                r.Add(new Vector2Int(x, newY));
+                                            }
+                                            else if (board[x, newY].type != ChessPieceType.Portal) //make sure we dont go through the portal repeatdly like infinite fall in portal
+                                            {
+                                                if (board[x, newY].team != team)
+                                                {
+                                                    r.Add(new Vector2Int(x, newY));
+                                                    goto LoopBreak; //if we hit an enemy we can stop, but we need to leave a few loops so just forcing the exit
+                                                }
+                                                else
+                                                {
+                                                    goto LoopBreak;
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            LoopBreak:
                 break;
             }
         }
@@ -43,9 +117,46 @@ public class Queen : ChessPiece
                 r.Add(new Vector2Int(i, currentY));
             if (board[i, currentY] != null)
             {
-                if (board[i, currentY].team != team)
+                if (board[i, currentY].team != team && board[i, currentY].type != ChessPieceType.Portal)
                     r.Add(new Vector2Int(i, currentY));
-
+                else if (board[i, currentY].type == ChessPieceType.Portal)
+                {
+                    for (int x = 0; x < tileCountX; x++)
+                    {
+                        for (int y = 0; y < tileCountY; y++)
+                        {
+                            if (x != i || y != currentY)
+                            {
+                                if (board[x, y] != null)
+                                {
+                                    if (board[x, y].type == ChessPieceType.Portal)
+                                    {
+                                        for (int newX = board[x, y].currentX; newX >= 0; newX--)
+                                        {
+                                            if (board[newX, y] == null)
+                                            {
+                                                r.Add(new Vector2Int(newX, y));
+                                            }
+                                            else if (board[newX, y].type != ChessPieceType.Portal) //make sure we dont go through the portal repeatdly like infinite fall in portal
+                                            {
+                                                if (board[newX, y].team != team)
+                                                {
+                                                    r.Add(new Vector2Int(newX, y));
+                                                    goto LoopBreak; //if we hit an enemy we can stop, but we need to leave a few loops so just forcing the exit
+                                                }
+                                                else
+                                                {
+                                                    goto LoopBreak;
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            LoopBreak:
                 break;
             }
         }
@@ -57,9 +168,46 @@ public class Queen : ChessPiece
                 r.Add(new Vector2Int(i, currentY));
             if (board[i, currentY] != null)
             {
-                if (board[i, currentY].team != team)
+                if (board[i, currentY].team != team && board[i, currentY].type != ChessPieceType.Portal)
                     r.Add(new Vector2Int(i, currentY));
-
+                else if (board[i, currentY].type == ChessPieceType.Portal)
+                {
+                    for (int x = 0; x < tileCountX; x++)
+                    {
+                        for (int y = 0; y < tileCountY; y++)
+                        {
+                            if (x != i || y != currentY)
+                            {
+                                if (board[x, y] != null)
+                                {
+                                    if (board[x, y].type == ChessPieceType.Portal)
+                                    {
+                                        for (int newX = board[x, y].currentX; newX < tileCountX; newX++)
+                                        {
+                                            if (board[newX, y] == null)
+                                            {
+                                                r.Add(new Vector2Int(newX, y));
+                                            }
+                                            else if (board[newX, y].type != ChessPieceType.Portal) //make sure we dont go through the portal repeatdly like infinite fall in portal
+                                            {
+                                                if (board[newX, y].team != team)
+                                                {
+                                                    r.Add(new Vector2Int(newX, y));
+                                                    goto LoopBreak; //if we hit an enemy we can stop, but we need to leave a few loops so just forcing the exit
+                                                }
+                                                else
+                                                {
+                                                    goto LoopBreak;
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                LoopBreak:
                 break;
             }
         }
@@ -71,9 +219,41 @@ public class Queen : ChessPiece
                 r.Add(new Vector2Int(x, y));
             else
             {
-                if (board[x, y].team != team)
+                if (board[x, y].team != team && board[x, y].type != ChessPieceType.Portal)
                     r.Add(new Vector2Int(x, y));
-
+                else if (board[x, y].type == ChessPieceType.Portal)
+                {
+                    for (int px = 0; px < tileCountX; px++)
+                    {
+                        for (int py = 0; py < tileCountY; py++)
+                        {
+                            if (px != x || py != y)
+                            {
+                                if (board[px, py] != null && board[px, py].type == ChessPieceType.Portal)
+                                {
+                                    for (int nx = px + 1, ny = py + 1; nx < tileCountX && ny < tileCountY; nx++, ny++)
+                                    {
+                                        if (board[nx, ny] == null)
+                                            r.Add(new Vector2Int(nx, ny));
+                                        else
+                                        {
+                                            if (board[nx, ny].type != ChessPieceType.Portal && board[nx, ny].team != team)
+                                            {
+                                                r.Add(new Vector2Int(nx, ny));
+                                                goto LoopBreak;
+                                            }
+                                            else
+                                            {
+                                                goto LoopBreak;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            LoopBreak:
                 break;
             }
         }
@@ -85,9 +265,41 @@ public class Queen : ChessPiece
                 r.Add(new Vector2Int(x, y));
             else
             {
-                if (board[x, y].team != team)
+                if (board[x, y].team != team && board[x, y].type != ChessPieceType.Portal)
                     r.Add(new Vector2Int(x, y));
-
+                else if (board[x, y].type == ChessPieceType.Portal)
+                {
+                    for (int px = 0; px < tileCountX; px++)
+                    {
+                        for (int py = 0; py < tileCountY; py++)
+                        {
+                            if (px != x || py != y)
+                            {
+                                if (board[px, py] != null && board[px, py].type == ChessPieceType.Portal)
+                                {
+                                    for (int nx = px - 1, ny = py + 1; nx >= 0 && ny < tileCountY; nx--, ny++)
+                                    {
+                                        if (board[nx, ny] == null)
+                                            r.Add(new Vector2Int(nx, ny));
+                                        else
+                                        {
+                                            if (board[nx, ny].type != ChessPieceType.Portal && board[nx, ny].team != team)
+                                            {
+                                                r.Add(new Vector2Int(nx, ny));
+                                                goto LoopBreak;
+                                            }
+                                            else
+                                            {
+                                                goto LoopBreak;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            LoopBreak:
                 break;
             }
         }
@@ -99,9 +311,41 @@ public class Queen : ChessPiece
                 r.Add(new Vector2Int(x, y));
             else
             {
-                if (board[x, y].team != team)
+                if (board[x, y].team != team && board[x, y].type != ChessPieceType.Portal)
                     r.Add(new Vector2Int(x, y));
-
+                else if (board[x, y].type == ChessPieceType.Portal)
+                {
+                    for (int px = 0; px < tileCountX; px++)
+                    {
+                        for (int py = 0; py < tileCountY; py++)
+                        {
+                            if (px != x || py != y)
+                            {
+                                if (board[px, py] != null && board[px, py].type == ChessPieceType.Portal)
+                                {
+                                    for (int nx = px + 1, ny = py - 1; nx < tileCountX && ny >= 0; nx++, ny--)
+                                    {
+                                        if (board[nx, ny] == null)
+                                            r.Add(new Vector2Int(nx, ny));
+                                        else
+                                        {
+                                            if (board[nx, ny].type != ChessPieceType.Portal && board[nx, ny].team != team)
+                                            {
+                                                r.Add(new Vector2Int(nx, ny));
+                                                goto LoopBreak;
+                                            }
+                                            else
+                                            {
+                                                goto LoopBreak;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            LoopBreak:
                 break;
             }
         }
@@ -112,9 +356,41 @@ public class Queen : ChessPiece
                 r.Add(new Vector2Int(x, y));
             else
             {
-                if (board[x, y].team != team)
+                if (board[x, y].team != team && board[x, y].type != ChessPieceType.Portal)
                     r.Add(new Vector2Int(x, y));
-
+                else if (board[x, y].type == ChessPieceType.Portal)
+                {
+                    for (int px = 0; px < tileCountX; px++)
+                    {
+                        for (int py = 0; py < tileCountY; py++)
+                        {
+                            if (px != x || py != y)
+                            {
+                                if (board[px, py] != null && board[px, py].type == ChessPieceType.Portal)
+                                {
+                                    for (int nx = px - 1, ny = py - 1; nx >= 0 && ny >= 0; nx--, ny--)
+                                    {
+                                        if (board[nx, ny] == null)
+                                            r.Add(new Vector2Int(nx, ny));
+                                        else
+                                        {
+                                            if (board[nx, ny].type != ChessPieceType.Portal && board[nx, ny].team != team)
+                                            {
+                                                r.Add(new Vector2Int(nx, ny));
+                                                goto LoopBreak;
+                                            }
+                                            else
+                                            {
+                                                goto LoopBreak;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            LoopBreak:
                 break;
             }
         }
